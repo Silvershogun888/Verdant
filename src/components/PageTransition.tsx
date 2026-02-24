937844d29121aeb0aa1daeb74d36e8e06aa3183f
Silvershogun888 <lweendomarco@gmail.com>
2026-02-24 21:39:59 +1300

    fix: resolve layout shift and whitespace issue by restoring wait mode

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -6,7 +6,7 @@ export const PageTransition = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <motion.div
         key={location.pathname}
         initial={{ opacity: 0, y: 10 }}
